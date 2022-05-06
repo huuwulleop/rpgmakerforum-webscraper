a6e43444a912738e55104d3fdbc7bd2b535ff514
VP <huuwulleop@hotmail.com>
2022-05-06 12:55:10 -0400

    Serialize to json file

--- index.py
+++ index.py
@@ -26,12 +26,13 @@ link = item_main.find(["li"], class_="structItem-startDate").a["href"]
 #     file.write(f"Link: {link}\n\n")
 # +++++++++++++++++++++++++++++++++++++++++++++++++++
 
+plugin_list = []
 
-with open("plugins_list.txt", "w") as file:
-    for i in range(1, 4):
+with open("plugins.json", "w") as file:
+    for i in range(1, 2):
         result = ""
         
-        file.write("\n\n")
+        # file.write("\n\n")
         
         if i == 1:
             result = requests.get(url)
@@ -42,15 +43,28 @@ with open("plugins_list.txt", "w") as file:
         plugin_page = doc.find(class_="structItemContainer-group js-threadList")
         plugins = plugin_page.find_all(["div"], class_=re.compile("structItem structItem--thread js-inlineModContainer js-threadListItem.*"))        
         
-        file.write(f"=== Page {i} ===\n\n")
+        # file.write(f"=== Page {i} ===\n\n")
     
         for plugin in plugins:
+            plugin_data = {}
+            
             main_div = plugin.find(class_="structItem-cell--main")
             
             title = main_div.find(class_="structItem-title").a
             author = main_div.find(class_="username").string
             link = main_div.find(["li"], class_="structItem-startDate").a["href"]
             
-            file.write(f"[{author}]\n")
-            file.write(f"\t{title.string}\n")
-            file.write(f"\t{dns}{link}\n\n")
+            plugin_data = {
+                "title": title.string,
+                "author": author,
+                "link": dns + link,
+            }
+                        
+            plugin_list.append(plugin_data)
+            
+            # file.write(f"[{author}]\n")
+            # file.write(f"\t{title.string}\n")
+            # file.write(f"\t{dns}{link}\n\n")
+            
+    json_obj = json.dumps(plugin_list, indent=4)
+    file.write(json_obj)
